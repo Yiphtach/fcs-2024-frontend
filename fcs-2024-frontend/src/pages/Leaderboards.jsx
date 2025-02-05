60837c813dde336cb02cb3a78c792a951adb0045
jephthe <jephthepetitfrere@gmail.com>
2025-02-04 22:24:12 -0500

    updated

--- fcs-2024-frontend/src/pages/Leaderboards.jsx
+++ fcs-2024-frontend/src/pages/Leaderboards.jsx
@@ -1,7 +1,7 @@
 // Leaderboard
 import React from 'react';
-import Header from '../frontend/src/components/Header';
-import Footer from '../frontend/src/components/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Leaderboard = ({ characters, currentPage, totalPages }) => {
   return (
